4cd33656fecf0c29e3ff32a8c738f00f3d94faa5
A5TUT0 <noahlezama2000@gmail.com>
2026-04-05 22:51:09 +0200

    Initial Commit

---
+++ PurgeMac/PurgeMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurgeMacApp.swift
+//  PurgeMac
+//
+//  Created by Noah Lezama on 05.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct PurgeMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
